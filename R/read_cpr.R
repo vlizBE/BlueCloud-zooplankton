8d8f8dab1c18f560d2706366add572db76f1679c
Salva <salvador.fernandez@vliz.be>
2020-03-04 17:38:28 +0100

    Log abundance calculated; Target species filtered

--- R/read_cpr.R
+++ R/read_cpr.R
@@ -15,8 +15,61 @@ data <- merge(events, occurrences, by = "id")
 var <- c("id", "occurrenceID", "eventDate",
          "decimalLatitude", "decimalLongitude", "geodeticDatum",
          "minimumDepthInMeters", "maximumDepthInMeters", "scientificName",
-         "scientificNameID", "individualCount")
+         "scientificNameID", "individualCount", "sampleSizeValue")
 data <- data[var]
+
+# Add aphiaID as another variable
+data$aphiaID <- as.integer(gsub("urn:lsid:marinespecies.org:taxname:", "", data$scientificNameID))
+
+# Get a dataset with only the present species
+species <- unique(
+  data.frame(
+    scientificName = data$scientificName, 
+    aphiaID = as.integer(gsub("urn:lsid:marinespecies.org:taxname:", "", data$scientificNameID))
+  )
+)
+
+# Set the target species - Include all species of the genuses Acartia and Oithona
+target_species <- data.frame(
+  scientificName = c("Temora longicornis", 
+                     "Calanus finmarchicus", 
+                     "Calanus helgolandicus", 
+                     "Metridia lucens"), 
+  aphiaID = as.integer(c(104878, 
+                         104464, 
+                         104466, 
+                         104633))
+)
+target_species <- rbind(
+  target_species, 
+  species[grep("Acartia", species$scientificName, ignore.case = TRUE), ],
+  species[grep("Oithona", species$scientificName, ignore.case = TRUE), ]
+)
+
+# Subset dataset to target species
+data <- subset(data, data$aphiaID %in% target_species$aphiaID)
+rm(list = c("species", "target_species"))
+
+# Convert factors to character vectors
+data[] <- lapply(data, as.character)
+
+# Low Acartia and Oithona species taxonomy to genus
+data$scientificName[grep("Acartia", data$scientificName, ignore.case = TRUE)] <- "Acartia"
+data$scientificNameID[grep("Acartia", data$scientificName, ignore.case = TRUE)] <- "urn:lsid:marinespecies.org:taxname:104108"
+data$aphiaID[grep("Acartia", data$scientificName, ignore.case = TRUE)] <- 104108
+
+data$scientificName[grep("Oithona", data$scientificName, ignore.case = TRUE)] <- "Oithona"
+data$scientificNameID[grep("Oithona", data$scientificName, ignore.case = TRUE)] <- "urn:lsid:marinespecies.org:taxname:106485"
+data$aphiaID[grep("Oithona", data$scientificName, ignore.case = TRUE)] <- 106485
+
+# Calculate number of individuals per cubic meter - new abundance column 
+data$abundance <- as.integer(data$individualCount) / as.integer(data$sampleSizeValue)
+data$abundanceUnit <- "ind/m3"
+data$abundanceMeasurementTypeID <- "http://vocab.nerc.ac.uk/collection/P01/current/SDBIOL01/"
+data$abundanceMeasurementUnitID <- "http://vocab.nerc.ac.uk/collection/P06/current/UPMM/"
+
+# Calculate the log abundance
+data$logAbundance <- log(data$abundance + 1)
 
 # Save as csv
 write.csv(data,
